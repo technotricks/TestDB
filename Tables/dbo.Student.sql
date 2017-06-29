ac6eef2b278b7d56dc0ca3e5d49ed2e2f3449337
technotricks <>
2017-06-29 14:33:42 +0800

    Add a new table Student

---
+++ Tables/dbo.Student.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Student] (
+		[id]       [int] NULL,
+		[name]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Student] SET (LOCK_ESCALATION = TABLE)
+GO
