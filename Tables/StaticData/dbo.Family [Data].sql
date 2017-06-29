1297cc017dfb340040451f2bbbffcda3fd08f72b
technotricks <>
2017-06-29 14:48:44 +0800

    add values in family table

---
+++ Tables/StaticData/dbo.Family [Data].sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Family] ON 
+
+INSERT [dbo].[Family] ([id], [Name]) VALUES (1, N'Kishore')
+INSERT [dbo].[Family] ([id], [Name]) VALUES (2, N'Raj')
+SET IDENTITY_INSERT [dbo].[Family] OFF
